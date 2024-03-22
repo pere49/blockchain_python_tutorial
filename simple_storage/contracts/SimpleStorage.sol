8198672e29a6cdd2875eae5cb62c08559c060930
pere49 <perekevin49@gmail.com>
2024-03-22 15:12:22 +0300

    variables and functions

---
+++ simple_storage/contracts/SimpleStorage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity >=0.6.0 <0.9.0;
+
+contract SimpleStorage {
+    // this will get initialized to 0
+    uint256 public favoriteNumber;
+
+    // bool favoriteBool = false;
+    // string favoriteString = "String";
+    // int256 favoriteInt = -5;
+    // address favoriteAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    // bytes32 favoriteBytes = "cat";
+
+    function store(uint256 _favoriteNumer) public {
+        favoriteNumber = _favoriteNumer;
+    }
+}
