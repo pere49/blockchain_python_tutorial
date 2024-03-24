4287c161c5481c1eacdbf30cd740828bfe9f20b5
pere49 <perekevin49@gmail.com>
2024-03-24 15:33:54 +0300

    get conversion rate eth => usd

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,21 +2,7 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
-// import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-interface AggregatorV3Interface {
-  function decimals() external view returns (uint8);
-  function description() external view returns (string memory);
-  function version() external view returns (uint256);
-
-  function getRoundData(
-    uint80 _roundId
-  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-
-  function latestRoundData()
-    external
-    view
-    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
-}
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract FundMe {
 
@@ -35,6 +21,13 @@ contract FundMe {
     function getPrice() public view returns(uint256) {
         AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         (,int256 answer,,,) = priceFeed.latestRoundData();
+        // value is to 10^8
         return uint256(answer);
+    }
+
+    function getConversionRate(uint256 ethAmount) public view returns (uint256) {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000;
+        return ethAmountInUsd;
     }
 }
