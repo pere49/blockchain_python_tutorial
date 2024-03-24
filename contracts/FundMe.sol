1b11d9d89a2c86bee05f3e57986a15087d0f9370
pere49 <perekevin49@gmail.com>
2024-03-24 15:05:07 +0300

    using interfaces to get latest price

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,11 +2,39 @@
 
 pragma solidity >=0.6.0 <0.9.0;
 
+// import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+interface AggregatorV3Interface {
+  function decimals() external view returns (uint8);
+  function description() external view returns (string memory);
+  function version() external view returns (uint256);
+
+  function getRoundData(
+    uint80 _roundId
+  ) external view returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+
+  function latestRoundData()
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract FundMe {
 
     mapping(address => uint256) public addressToAmountFunded;
     
     function fund() public payable {
         addressToAmountFunded[msg.sender] += msg.value;
+        // determine the conversion rate 
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    function getPrice() public view returns(uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 answer,,,) = priceFeed.latestRoundData();
+        return uint256(answer);
     }
 }
